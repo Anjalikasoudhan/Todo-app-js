2e45ec112934183a485d8242c144cd9df76c7f79
Anjali Kasoudhan <anjalikasoudhan1705@gmail.com>
2026-01-17 21:49:20 +0530

    Implement add and delete todo functionality

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const todoInput=document.getElementById("todoInput");
 const addBtn=document.getElementById("addBtn");
 const todoList=document.getElementById("todoList");
+
+addBtn.addEventListener('click',function(){
+
+    const input=todoInput.value;
+    if(input=="")return;
+
+    const list=document.createElement("li");
+    list.innerText=input;
+    todoList.appendChild(list);
+    console.log(todoList);
+
+    const deletebtn=document.createElement("button");
+    deletebtn.innerText="Delete";
+    list.appendChild(deletebtn);
+    deletebtn.addEventListener('click',()=>{
+        list.remove();
+    });
+
+    todoInput.value="";
+})
