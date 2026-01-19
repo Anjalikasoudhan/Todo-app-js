5bb51534c112ad2a6aebea6d82d35cd2efb9c3fa
Anjali Kasoudhan <anjalikasoudhan1705@gmail.com>
2026-01-19 23:06:28 +0530

    Add completed toggle and manage todos using array state

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const todoInput=document.getElementById("todoInput");
 const addBtn=document.getElementById("addBtn");
 const todoList=document.getElementById("todoList");
+let todos=[];
+
 
 addBtn.addEventListener('click',function(){
 
     const input=todoInput.value;
     if(input=="")return;
-
+    const obj={text:input,
+               completed:false};
+    todos.push(obj);
+    console.log(todos);
+    
+    
+     todos.push=obj;
+     console.log(todos);
+   
     const list=document.createElement("li");
-    list.innerText=input;
-    todoList.appendChild(list);
-    console.log(todoList);
+    list.innerText=obj.text;
+   
+    list .addEventListener('click',()=>{
+    list.classList.toggle("completed");
+    obj.completed = !obj.completed;
+    console.log(obj);
+    });
 
+    
     const deletebtn=document.createElement("button");
     deletebtn.innerText="Delete";
     list.appendChild(deletebtn);
+    
     deletebtn.addEventListener('click',()=>{
         list.remove();
     });
-
+     todoList.appendChild(list);
     todoInput.value="";
 })
